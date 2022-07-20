fc923b6b919062a8b062ccbc68ce9c234f44956e
minhaneul <onlynforever@gmail.com>
2022-07-20 13:23:48 +0900

    Create: flutter instagram

---
+++ lib/root_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:instagram/tab_page.dart';
+
+class RootPage extends StatelessWidget {
+  const RootPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const TabPage();
+  }
+}
